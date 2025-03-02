ae35064a9bd54fd4d1a4bd4229673e34b0a6c98c
Shariq Riaz <ishariqriaz@gmail.com>
2025-03-03 01:43:54 +0500

    loadbalanceaa

--- webview-ui/src/__mocks__/lucide-react.ts
+++
@@ -1,6 +0,0 @@
-import React from "react"
-
-export const Check = () => React.createElement("div")
-export const ChevronsUpDown = () => React.createElement("div")
-export const Loader = () => React.createElement("div")
-export const X = () => React.createElement("div")
